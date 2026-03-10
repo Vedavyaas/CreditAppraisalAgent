63fc0ef5da1d74bf2c6ddb7d1a039ce356cd2cd1
Vedavyaas M R <itsvyaas32@gmail.com>
2026-03-10 21:25:13 +0530

    Final commit

--- src/main/java/com/pheonix/creditappraisalmemo/controller/HealthController.java
+++ src/main/java/com/pheonix/creditappraisalmemo/controller/HealthController.java
@@ -1,11 +1,23 @@
 package com.pheonix.creditappraisalmemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class HealthController {
+
+    @Value("${frontend.url:http://localhost:3000}")
+    private String frontendUrl;
+
+    @GetMapping("/")
+    public void root(HttpServletResponse response) throws IOException {
+        response.sendRedirect(frontendUrl);
+    }
 
     @GetMapping("/api/health")
     public ResponseEntity<String> health() {
